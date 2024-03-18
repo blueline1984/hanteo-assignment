13a4370cbbb49d4dbfb9860d9b80eb54a2fa0443
been <blueline1318@gmail.com>
2024-03-18 13:55:18 +0900

    feature: set routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/chart" />
+        <Route path="/events" />
+        <Route path="/news" />
+        <Route path="/store" />
+        <Route path="/recharge" />
+      </Routes>
     </>
   );
 }
